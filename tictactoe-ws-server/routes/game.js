183e068845b80b57e2b9ea500f19c0986c9ab698
Shahar Tal <39523779+chapost1@users.noreply.github.com>
2018-08-04 00:11:24 +0300

    added logic to computer
    now not so easy to win tough.. :)

--- tictactoe-ws-server/routes/game.js
+++ tictactoe-ws-server/routes/game.js
@@ -13,11 +13,12 @@ router.get('/opensocket', function (req, res, next) {
             console.log('Received:' + msg);
             var arr = JSON.parse(msg).game;
             try {
-                let newArr = await findEmptySpace(arr);
-                console.log("" + newArr);
-                setTimeout(()=>{
-                    ws.send(JSON.stringify({ player: "computer", game: newArr, date: new Date() }));
-                },500);
+
+                let newArr = await playTheGame(arr);
+
+                setTimeout(() => {
+                    ws.send(JSON.stringify({ player: "computer", game: newArr}));
+                }, 500);
             } catch (e) {
                 console.log("" + e)
                 ws.send(JSON.stringify({ error: e }));
@@ -32,7 +33,24 @@ router.get('/opensocket', function (req, res, next) {
 });
 
 
-
+function playTheGame(arr) {
+    return new Promise(async function(resolve ,reject) {
+        try {
+            let newArr = await preventLosing(arr);
+            console.log(newArr[0]);
+            if (!(newArr[0])) {
+                newArr = await findEmptySpace(arr);
+                console.log('plan B: ' + newArr);
+            } else {
+                newArr = newArr[1];
+                console.log('plan A: ' + newArr);
+            };
+            resolve(newArr);
+        } catch(e){
+            reject(e);
+        }
+    });
+}
 
 function findEmptySpace(arr) {
 
@@ -83,6 +101,47 @@ function randomWithRange(min, max) {
     let range = (max - min) + 1;
     return Math.floor(Math.random() * range) + min;
 };
+
+
+
+
+
+function preventLosing(arr) {
+    return new Promise((resolve) => {
+        const conditions = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // colums
+            [0, 4, 8], [2, 4, 6]             // diagonal 
+        ];
+        for (let condition of conditions) {
+            if (arr[condition[0]] == 'X' && arr[condition[1]] == 'X' && arr[condition[2]] != '0' ||
+                arr[condition[0]] == '0' && arr[condition[1]] == '0' && arr[condition[2]] != 'X') {
+                resolveHere(2, condition);
+                break;
+            }
+            if (arr[condition[1]] == 'X' && arr[condition[2]] == 'X' && arr[condition[0]] != '0' ||
+                arr[condition[1]] == '0' && arr[condition[2]] == '0' && arr[condition[0]] != 'X') {
+                resolveHere(0, condition);
+                break;
+            }
+            if (arr[condition[0]] == 'X' && arr[condition[2]] == 'X' && arr[condition[1]] != '0' ||
+                arr[condition[0]] == '0' && arr[condition[2]] == '0' && arr[condition[1]] != 'X') {
+                resolveHere(1, condition);
+                break;
+            }
+        }
+        resolve([false, arr]);
+
+        function resolveHere(i, condition) {
+            if (arr[arr[condition[i]]] != 'X' && arr[arr[condition[i]]] != '0') {
+                arr[condition[i]] = '0';
+                console.log(condition[i]);
+            }
+            resolve([true, arr]);
+        }
+    });
+}
+
 
 
 
